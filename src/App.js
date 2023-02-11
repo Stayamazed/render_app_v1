20bacd6398841b5f860e4349c0e27eb2e9c7afbd
Stayamazed <125111999+Stayamazed@users.noreply.github.com>
2023-02-11 05:29:19 +0000

    First commit of render_app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
-import './App.css';
+import {
+  ChakraProvider,
+  Heading,
+  Container,
+  Text,
+  Input,
+  Button,
+  Wrap,
+  Stack, 
+  Image,
+  SkeletonCircle,
+  SkeletonText,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [image, updateImage] = useState();
+  const [prompt, updatePrompt] = useState();
+  const [loading, updateLoading] = useState();
+
+  const generate = async (prompt) => {
+    updateLoading(true);
+    const result = await axios.get(`http://127.0.0.1:8000/?prompt=${prompt}`);
+    updateImage(result.data.output[0]);
+    updateLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Container padding={"50px"}>
+        <Heading>Stable Diffusion Playground</Heading>
+        <Text marginBottom={"10px"}>
+          Just enter the prompt and click the generate button, No code required to generate the image.
+        </Text>
+
+        <Wrap marginBottom={"10px"}>
+          <Input
+            value={prompt}
+            onChange={(e) => updatePrompt(e.target.value)}
+            width={"350px"}
+          ></Input>
+          <Button onClick={(e) => generate(prompt)} colorScheme={"yellow"}>
+            Generate
+          </Button>
+        </Wrap>
+
+        {loading ? (
+          <Stack>
+            <SkeletonCircle />
+            <SkeletonText />
+          </Stack>
+        ) : image ? (
+          <Image src={image} boxShadow="lg" />
+        ) : null}
+      </Container>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
